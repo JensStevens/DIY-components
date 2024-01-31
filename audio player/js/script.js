ea00ac289a4641725a875e3c01ccce6550e86b39
JensStevens <stevens.jens@hotmail.com>
2024-02-01 00:16:21 +0100

    song array

---
+++ audio player/js/script.js
@@ -0,0 +1,38 @@
+const song = [
+    {
+        title: "Days Before Rodeo/The Prayer",
+        artist: "Travis Scott",
+        url: "../assets/audio/ThePrayer.mp3",
+        cover: "../assets/img/dbr.jpg"
+    },
+    {
+        title: "Backyard",
+        artist: "Travis Scott",
+        url: "../assets/audio/Backyard.mp3",
+        cover: "../assets/img/dbr.jpg"
+    },
+    {
+        title: "6 God",
+        artist: "Drake",
+        url: "../assets/audio/6God.mp3",
+        cover: "../assets/img/IYRTITL.jpg"
+    },
+    {
+        title: "FML (O.G. Version Feat. The Weeknd, Bon Iver & Travis Scott)",
+        artist: "Kanye West",
+        url: "../assets/audio/FML-OG.mp3",
+        cover: "../assets/img/kanye.jpg"
+    },
+    {
+        title: "Days In The East",
+        artist: "Drake",
+        url: "../assets/audio/DaysInTheEast.mp3",
+        cover: "../assets/img/IYRTITL.jpg"
+    },
+    {
+        title: "Drugs You Should Try",
+        artist: "Travis Scott",
+        url: "../assets/audio/DrugsYouShouldTry.mp3",
+        cover: "../assets/img/dbr.jpg"
+    }
+];
