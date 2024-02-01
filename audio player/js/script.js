2569474afa8f05863f7dcc1175077c7df1b153f5
JensStevens <stevens.jens@hotmail.com>
2024-02-01 01:27:20 +0100

    icons

--- audio player/js/script.js
+++ audio player/js/script.js
@@ -1,54 +1,51 @@
 const song = [
-    {
-        title: "Days Before Rodeo/The Prayer",
-        artist: "Travis Scott",
-        url: "../assets/audio/ThePrayer.mp3",
-        cover: "../assets/img/dbr.jpg"
-    },
-    {
-        title: "Backyard",
-        artist: "Travis Scott",
-        url: "../assets/audio/Backyard.mp3",
-        cover: "../assets/img/dbr.jpg"
-    },
-    {
-        title: "6 God",
-        artist: "Drake",
-        url: "../assets/audio/6God.mp3",
-        cover: "../assets/img/IYRTITL.jpg"
-    },
-    {
-        title: "FML (O.G. Version Feat. The Weeknd, Bon Iver & Travis Scott)",
-        artist: "Kanye West",
-        url: "../assets/audio/FML-OG.mp3",
-        cover: "../assets/img/kanye.jpg"
-    },
-    {
-        title: "Days In The East",
-        artist: "Drake",
-        url: "../assets/audio/DaysInTheEast.mp3",
-        cover: "../assets/img/IYRTITL.jpg"
-    },
-    {
-        title: "Drugs You Should Try",
-        artist: "Travis Scott",
-        url: "../assets/audio/DrugsYouShouldTry.mp3",
-        cover: "../assets/img/dbr.jpg"
-    }
+  {
+    title: "Days Before Rodeo/The Prayer",
+    artist: "Travis Scott",
+    url: "../assets/audio/ThePrayer.mp3",
+    cover: "../assets/img/dbr.jpg",
+  },
+  {
+    title: "Backyard",
+    artist: "Travis Scott",
+    url: "../assets/audio/Backyard.mp3",
+    cover: "../assets/img/dbr.jpg",
+  },
+  {
+    title: "6 God",
+    artist: "Drake",
+    url: "../assets/audio/6God.mp3",
+    cover: "../assets/img/IYRTITL.jpg",
+  },
+  {
+    title: "FML (O.G. Version Feat. The Weeknd, Bon Iver & Travis Scott)",
+    artist: "Kanye West",
+    url: "../assets/audio/FML-OG.mp3",
+    cover: "../assets/img/kanye.jpg",
+  },
+  {
+    title: "Days In The East",
+    artist: "Drake",
+    url: "../assets/audio/DaysInTheEast.mp3",
+    cover: "../assets/img/IYRTITL.jpg",
+  },
+  {
+    title: "Drugs You Should Try",
+    artist: "Travis Scott",
+    url: "../assets/audio/DrugsYouShouldTry.mp3",
+    cover: "../assets/img/dbr.jpg",
+  },
 ];
-
-
-
-
-
-
 
 // drag import
 
 dragElement(document.getElementById("audio-player"));
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
   if (document.getElementById(elmnt.id + "controls")) {
     // if present, the controls is where you move the DIV from:
     document.getElementById(elmnt.id + "controls").onmousedown = dragMouseDown;
@@ -77,8 +74,8 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
   }
 
   function closeDragElement() {
