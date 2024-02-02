16db39a81a2022c77ca8096bce3a802f07e81d92
JensStevens <stevens.jens@hotmail.com>
2024-02-02 13:10:10 +0100

    progress bar transition polish

--- audio player/js/script.js
+++ audio player/js/script.js
@@ -113,6 +113,7 @@ play.addEventListener("click", function () {
     play.classList.add("playing");
     play.classList.remove("pause");
     progressContainer.style.opacity = "1";
+    progressContainer.style.transition = "opacity 0.5s ease-in 0.5s";
     artistTitleContainer.style.opacity = "1";
     for (let i = 0; i < controls.length; i++) {
       controls[i].style.marginBottom = "16px";
@@ -125,6 +126,7 @@ play.addEventListener("click", function () {
     play.classList.remove("playing");
     play.classList.add("pause");
     progressContainer.style.opacity = "0";
+    progressContainer.style.transition = "opacity 0.2s ease-in 0.2s";
     artistTitleContainer.style.opacity = "0";
     for (let i = 0; i < controls.length; i++) {
       controls[i].style.marginBottom = "0";
